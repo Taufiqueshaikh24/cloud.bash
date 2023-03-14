3ef96b311dae9fb2a87df234220c16c352cd1c9d
Taufiqueshaikh24 <88907221+Taufiqueshaikh24@users.noreply.github.com>
2023-03-14 16:55:16 +0530

    Create OpenStack.bash

---
+++ OpenStack.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo useradd -s /bin/bash -d /opt/stack -m stack
+sudo chmod +x /opt/stack
+echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/stack
+sudo -u stack -i
+git clone https://opendev.org/openstack/devstack
+cd devstack
+cat > local.conf
+[[local|localrc]]
+ADMIN_PASSWORD=secret
+DATABASE_PASSWORD=$ADMIN_PASSWORD
+RABBIT_PASSWORD=$ADMIN_PASSWORD
+SERVICE_PASSWORD=$ADMIN_PASSWORD
+./stack.sh
